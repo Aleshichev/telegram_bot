e6c052ce0803c8fa256e5d3fd6fe8755f1bebf35
Igor <Aleshichev.git@email.ua>
2022-07-03 17:59:14 +0300

    подготовка ответы

--- bot_telegram.py
+++ bot_telegram.py
@@ -5,7 +5,33 @@ from aiogram.utils import executor
 import os
 
 bot = Bot(token=os.getenv('TOKEN'))
-dp  = Dispatcher(bot)
+dp = Dispatcher(bot)
+
+async def on_startup(_):
+    print('Бот вышел в онлайн')
+
+#------------------------------ Клиентская часть
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message : types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/Pizza_Sheffbot')
+
+@dp.message_handler(commands=['Режим_работы'])
+async def pizza_open_command(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00')
+
+
+@dp.message_handler(commands=['Расположение'])
+async def pizza_place_command(message: types.Message):
+    await bot.send_message(message.from_user.id, 'ул. Нигера 11')
+
+
+#------------------------------- Админская часть
+#--------------------------------- Общая часть
+
 
 @dp.message_handler()
 async def echo_send(message : types.Message):
@@ -16,4 +42,4 @@ async def echo_send(message : types.Message):
     #  await bot.send_message(message.from_user.id, message.text)
 
 
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
