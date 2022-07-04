83379cd2cff38cf2b907a8652be8b68c888ab432
Igor <Aleshichev.git@email.ua>
2022-07-04 19:21:20 +0300

    админка

--- bot_telegram.py
+++ bot_telegram.py
@@ -7,7 +7,9 @@ async def on_startup(_):
 from handlers import client, admin, other
 
 client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
 other.register_hadlers_other(dp)
+
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
